ef808c24f781cc16df1d24c817a008a3f46dbf22
yogindra3926 <indraeswar12902@gmail.com>
2024-07-10 17:33:36 +0530

    second coommit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import {BrowserRouter} from "react-router-dom"
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { SocketProvider } from './context/SocketProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
+    <SocketProvider>
     <App />
+    </SocketProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
